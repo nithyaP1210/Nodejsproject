6e57d31cdb4cd643f13dc0105c53c1e702ab1b6b
23wh1a1210 <150106894+nithyaP1210@users.noreply.github.com>
2025-04-13 17:00:03 +0530

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -1,10 +1,10 @@
 import "./styles.css";
-
 import "./styles.css";
 import React from "react";
+
 export default function Resume() {
   return (
-    <div className=" resume-container min-h-screen bg-gray-100 p-6 font-sans">
+    <div className="resume-container min-h-screen bg-gray-100 p-6 font-sans">
       <div className="max-w-4xl mx-auto bg-white p-8 shadow-2xl rounded-2xl">
         <h1 className="text-4xl font-bold mb-2 text-gray-800">
           Penmetsa Nithya
@@ -14,8 +14,8 @@ export default function Resume() {
         <section className="mb-6">
           <h2 className="text-2xl font-semibold text-gray-700 mb-2">Contact</h2>
           <p>Email: 23wh1a1210@bvrithyderabad.edu.in</p>
-          <p>Phone: (123) 456-7890</p>
-          <p>Location: San Francisco, CA</p>
+          <p>Phone: 9876543210</p>
+          <p>Location: Hyderabad, India</p>
         </section>
 
         <section className="mb-6">
@@ -41,7 +41,7 @@ export default function Resume() {
           <h2 className="text-2xl font-semibold text-gray-700 mb-2">
             Education
           </h2>
-          <div>
+          <div className="mb-4">
             <h3 className="text-xl font-bold text-gray-800">B.Tech</h3>
             <p className="text-gray-600">BVRIT Hyderabad – 2023 to Present</p>
             <ul className="list-disc list-inside">
@@ -52,13 +52,37 @@ export default function Resume() {
               </li>
             </ul>
           </div>
+
+          <div>
+            <h3 className="text-xl font-bold text-gray-800">Intermediate</h3>
+            <p className="text-gray-600">
+              {" "}
+              Vinjee Junior College – 2021 to 2023
+            </p>
+            <ul className="list-disc list-inside">
+              <li>Percentage - 96.7%</li>
+            </ul>
+          </div>
         </section>
 
         <section>
           <h2 className="text-2xl font-semibold text-gray-700 mb-2">
-            Intermediate
+            Certifications
           </h2>
-          <p>Percentage - 96.7%</p>
+          <ul className="list-disc list-inside">
+            <li>Full Stack Web Development - NXTWave Academy</li>
+            <li>Joy of computing using Python - NPTEL</li>
+            <li>SemiConductor Fabrication - Purdue University</li>
+          </ul>
+        </section>
+        <section>
+          <h2 className="text-2xl font-semibold text-gray-700 mb-2">
+            Co-curricular
+          </h2>
+          <ul className="list-disc list-inside">
+            <li>Videographer at the film club </li>
+            <li>Completed level -  1 at Vishnu School of music</li>
+          </ul>
         </section>
       </div>
     </div>
